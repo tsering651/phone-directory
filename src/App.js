4083e0bae9dc05241838401543d07899f10525f3
tsering651 <tseringwang4039@gmail.com>
2022-09-16 11:32:23 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
-
 function App() {
   return (
-    <div >
-      Phone Directory
+    <div>
+      {/* <div className="header">Phone Directory</div>
+      <button>Add</button>
+      <div>
+        <span>Name</span>
+        <br></br>
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name:</label>
+      <input id="name" type="text"  placeholder="Type here" defaultValue="Tsering"/>
     </div>
   );
 }
