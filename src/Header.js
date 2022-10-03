b8ca5a7bcc53f7fe0d77673ea77f798e118f45b6
tsering651 <tseringwang4039@gmail.com>
2022-10-03 11:27:28 +0530

    styling done using inline css

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import "./Header.css"
 
 const Header=function(){
+   // const headerStyle={textAlign:"center",padding:20,backgroundColor:"#000",color:"#fff",textTransform:"uppercase"};
     return(
-        <div className="header">Phone Directory</div>  
+        <div className="header" >
+            Phone Directory
+        </div>  
     )
 }
 export default Header;
