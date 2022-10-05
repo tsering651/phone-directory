8966a20ae9c980d21af3577cf5c8c923915ad683
tsering651 <tseringwang4039@gmail.com>
2022-10-05 15:13:19 +0530

    passed heading to header component using props

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Header.css"
 
-const Header=function(){
+const Header=function(props){
    // const headerStyle={textAlign:"center",padding:20,backgroundColor:"#000",color:"#fff",textTransform:"uppercase"};
     return(
         <div className="header" >
-            Phone Directory
+            {props.heading} 
         </div>  
     )
 }
